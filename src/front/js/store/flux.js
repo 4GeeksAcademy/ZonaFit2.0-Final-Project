d6771db1c6318b6e4261ff9885d15fa08938c662
AdrsP <144963769+AdrsP@users.noreply.github.com>
2024-02-06 19:49:39 +0000

    se agrego el fetch login, correcciones a la base de datos

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: "",
+			currentUser: "",
 
 		},
 		actions: {
@@ -26,7 +28,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 
+			iniciarSesion: async (email, password) =>{
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "api/login",{
+						method: 'POST',
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							"email": email,
+							"password": password,
+						})
+					})
 
+					if (resp.status !== 200){
+						alert("Error en los datos");
+						return false
+					}
+					alert("logueado exitosamente")
+					const data = await resp.json();
+					localStorage.setItem("token", data.access_token)
+					setStore({token: data.access_token})
+					setStore({currentUser: email})
+
+				} catch (error) {
+					console.error("hubo un error en el login", error)
+				}
+			},
 		}
 	};
 };
