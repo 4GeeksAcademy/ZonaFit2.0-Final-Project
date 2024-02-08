8ab53a9a5cbd677361207043be97354f5fec969e
AdrsP <144963769+AdrsP@users.noreply.github.com>
2024-02-08 15:20:07 +0000

    revision de prueba y cambio de estilos

--- src/front/js/component/RoutineCard.js
+++ src/front/js/component/RoutineCard.js
@@ -11,11 +11,11 @@ export const RoutineCard = (props) => {
         <div className="card text-bg-dark p-0">
             <img src={props.img} className="card-img" alt="..."/>
                 <div className="card-img-overlay">
-                    <h5 className="card-title mb-1">{props.nivel}</h5>
-                    <p className="text-left mb-2">{props.nombre}</p>
-                    <p className="text-left mb-0">Equipo completo</p>
-                    <p className="text-left mb-0">{props.tipoDeEntrenamiento}</p>
-                    <p className="text-left mb-1">Total rutinas: 2</p>
+                    <h5 className="card-title text-light fw-bold mb-1">{props.nivel}</h5>
+                    <p className="text-left text-light mb-2">{props.nombre}</p>
+                    <p className="text-left text-light mb-0">Equipo completo</p>
+                    <p className="text-left text-light mb-0">{props.tipoDeEntrenamiento}</p>
+                    <p className="text-left text-light mb-1">Total rutinas: 2</p>
                     <div className="d-flex flex-row-reverse mt-1">
                         <button className="btn btn-warning">Iniciar</button>
                     </div>
