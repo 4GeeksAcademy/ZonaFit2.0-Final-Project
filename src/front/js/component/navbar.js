70f6f0ebcd32b984ce73b3674ee33479854e094a
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2024-02-09 00:39:00 +0000

    se agrego el navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import logo from "../../img/logo.png"
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav classname="navbar navbar-expand-md  navbar-dark fixed-top">
+
+      <a classname="navbar-brand" href="#home"><img src="../../img/logo.png" /></a>
+
+      <a classname="navbar-toggler togg" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+
+        <i classname="fas fa-bars menu "></i>
+      </a>
+
+
+      <div classname="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul classname="navbar-nav navbar-t">
+          <li classname="nav-item">
+            <a classname="nav-link" href="#home">Home</a>
+          </li>
+          <li classname="nav-item">
+            <a classname="nav-link" href="#about">sobre nosotros</a>
+          </li>
+          <li classname="nav-item">
+            <a classname="nav-link" href="#class">Clases</a>
+          </li>
+          <li classname="nav-item">
+            <a classname="nav-link" href="#sch">Schedules</a>
+          </li>
+          <li classname="nav-item">
+            <a classname="nav-link" href="#contact">Contacto</a>
+          </li>
+        </ul>
+        <ul classname="navbar-nav navbar-social ">
+          <a href=""><i classname="fab fa-facebook-f"></i></a>
+          <a href=""> <i classname="fab fa-instagram"></i></a>
+          <i classname="fab fa-twitter"> </i>
+
+        </ul>
+
+      </div>
+    </nav>
+  );
 };
