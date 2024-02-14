f73bfe7966fc290d980eac3a6eda5179ad09cb19
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2024-02-14 18:04:39 +0000

    recetas

--- src/front/js/pages/home/encabezado.js
+++ src/front/js/pages/home/encabezado.js
@@ -7,22 +7,22 @@ export const Encabezado = () => {
 
 
     return (
-        <div class="home ">
-        <div className="home-content" id="home">
+        <div className="home ">
+            <div className="home-content" id="home">
 
-            <h3 data-aos="fade-right" data-aos-duration="1000">
-                es hora de construir un mejor cuerpo!
-            </h3>
-            <h2 data-aos="fade-right"
-                data-aos-duration="1000">Cambia tu cuerpo con
-                Zonafit2.0 </h2>
-            <div className="home-btn">
-                <button className="btnl bt" data-aos="fade-left"
-                    data-aos-duration="1000">empezemos</button>
-                <button className="btnl bt" data-aos="fade-down"
-                    data-aos-duration="1000">mas info</button>
+                <h3 data-aos="fade-right" data-aos-duration="1000">
+                    es hora de construir un mejor cuerpo!
+                </h3>
+                <h2 data-aos="fade-right"
+                    data-aos-duration="1000">Cambia tu cuerpo con
+                    Zonafit2.0 </h2>
+                <div className="home-btn">
+                    <button className="btnl bt" data-aos="fade-left"
+                        data-aos-duration="1000">empezemos</button>
+                    <button className="btnl bt" data-aos="fade-down"
+                        data-aos-duration="1000">mas info</button>
+                </div>
             </div>
         </div>
-</div>
     )
 }
